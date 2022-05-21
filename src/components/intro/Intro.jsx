a9fe0ba94e5a70b5a4d56c054919cef4bac55ce0
Gustavo Tavares <gmartinez-de-oliveir@myseneca.ca>
2022-05-21 18:07:42 -0400

    Intro with animation

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,9 +1,41 @@
 import "./intro.scss";
+import { init } from "ityped";
+
+import { ArrowDownward } from "@material-ui/icons";
+import { useEffect, useRef } from "react";
 
 export const Intro = () => {
+  const text = useRef();
+  useEffect(() => {
+    init(text.current, {
+      showCursor: true,
+      backDelay: 1500,
+      backSpeed: 60,
+      strings: ["Developer", "Programmer"],
+    });
+  }, []);
+
   return (
     <div className="intro" id="intro">
-      intro
+      <div className="left">
+        <div className="wrapper">
+          <h2>Hello! I'm</h2>
+          <h1>Gustavo Tavares</h1>
+          <h3>
+            Junior <span ref={text}></span>
+          </h3>
+        </div>
+
+        <a href="#portfolio">
+          <ArrowDownward className="down" />
+        </a>
+      </div>
+
+      <div className="right">
+        <div className="imgContainer">
+          <img src="assets/logopng.PNG" alt="" />
+        </div>
+      </div>
     </div>
   );
 };
