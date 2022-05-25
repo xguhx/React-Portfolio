e7c6acf694673761c2fc11d698d9856c77c25c0b
Gustavo Tavares <gmartinez-de-oliveir@myseneca.ca>
2022-05-25 15:04:30 -0400

    Fixing Intro

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -5,7 +5,6 @@ import { ArrowDownward } from "@material-ui/icons";
 import { useEffect, useRef, useState } from "react";
 
 export const Intro = () => {
-  const [mobile, setMobile] = useState(false);
   const text = useRef();
   useEffect(() => {
     init(text.current, {
@@ -16,43 +15,17 @@ export const Intro = () => {
     });
   }, []);
 
-  const handleResize = () => {
-    if (window.innerWidth < 768) {
-      setMobile(true);
-    } else {
-      setMobile(false);
-    }
-  };
-  // create an event listener
-  useEffect(() => {
-    window.addEventListener("resize", handleResize);
-  });
-
   return (
     <div className="intro" id="intro">
       <div className="left">
         <div className="wrapper">
-          {!mobile ? (
-            <>
-              <h2>Hello! I'm Gustavo Tavares</h2>
-              <h3>
-                I am a <span ref={text}></span>Developer{" "}
-              </h3>{" "}
-            </>
-          ) : (
-            <>
-              <h2>
-                Hello! I'm <br />
-                Gustavo Tavares
-              </h2>
-              <h3>
-                I am a <br />
-                <span ref={text}></span>
-                <br />
-                Developer{" "}
-              </h3>{" "}
-            </>
-          )}
+          <h2>Hello! I'm </h2>
+          <h2> Gustavo Tavares</h2>
+          <h3>
+            I am a <br />
+            <span ref={text}></span>
+          </h3>
+          <h3>Developer </h3>
         </div>
 
         <a href="#works">
