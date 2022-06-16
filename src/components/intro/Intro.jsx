1ab8ae984711b20acb490c6efa9796591adcae5d
Gustavo Tavares <gmartinez-de-oliveir@myseneca.ca>
2022-06-16 16:45:47 -0400

    Changing Intro

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -11,7 +11,7 @@ export const Intro = () => {
       showCursor: true,
       backDelay: 1500,
       backSpeed: 60,
-      strings: ["Front-end", "Back-end", "Mobile"],
+      strings: ["Full-Stack", "Front-End", "Back-End"],
     });
   }, []);
 
